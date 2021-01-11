b3e3b60e2ff65199bc8c6f831ebd548f9a8a9be3
rojae <Jae.Thon96@gmail.com>
2021-01-11 22:15:22 +0900

    Add) Pageable and Sort

--- src/main/java/com/example/demo/post/PostController.java
+++ src/main/java/com/example/demo/post/PostController.java
@@ -1,6 +1,8 @@
 package com.example.demo.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +26,10 @@ public class PostController {
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable("id") Post post) {
         return post.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public Page<Post> getPosts(Pageable pageable){
+        return postRepository.findAll(pageable);
     }
 }
--- src/test/java/com/example/demo/PostControllerTest.java
+++ src/test/java/com/example/demo/PostControllerTest.java
@@ -10,10 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -38,4 +38,21 @@ public class PostControllerTest {
                 .andExpect(content().string("Test title"));
     }
 
+    @Test
+    public void getPosts() throws Exception {
+        Post post = new Post();
+        post.setTitle("Test title");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/")
+                .param("page","0")
+                .param("size","10")
+                .param("sort","created,desc")
+                .param("sort","title"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(result -> {
+                    jsonPath("$.content[0].title", is("Test title"));
+                });
+    }
 }
