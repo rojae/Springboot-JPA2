5fa89f664e6c76897867ea2fc258535b3334b413
rojae <Jae.Thon96@gmail.com>
2021-01-19 21:28:54 +0900

    Add) closed & open projection of repository

--- src/main/java/com/example/demo/comment/CommentSummary.java
+++ src/main/java/com/example/demo/comment/CommentSummary.java
@@ -9,10 +9,9 @@ public interface CommentSummary {
 
     int getDown();
 
-    // Comment의 up 컬럼과 down 컬럼을 합침
-    // target은 comment이기 때문에, comment를 모두 가져올 수 밖에 없다
-    // 즉, open projection
-    @Value("#{target.up + ' ' + target.down}")
-    String getVotes();
+    default String getVotes() {
+        return getUp() + " " + getDown();
+    }
+
 }
 
